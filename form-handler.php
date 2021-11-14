017c5c3a5b314be8c13334c65fdf8fe3fb6532c4
imtiazimran <81389032+imtiazimran@users.noreply.github.com>
2021-11-15 02:32:41 +0600

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@eduford.com';
+$email_subject = ' New form submission';
+
+$email_body = "User Name: $name.\n".
+               "User email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                 "User_massege: $message.\n".;
+ 
+                 
+$to = 'amranhossain080@gmail.com';
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-to: $visitor_email \r\n";
+
+email($to,$email_subject,$email_body,$headers);
+
+
+header("Location: contact.html")
+
+
+?>
